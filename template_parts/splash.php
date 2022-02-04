5509e0a4740c98c30742658ed77779b98b921f78
LinuxPony <sonic.ert@gmail.com>
2022-02-04 04:03:41 -0800

    - Swapped out splash BG and source link - Bumped version to 1.0.10

--- template_parts/splash.php
+++ template_parts/splash.php
@@ -3,6 +3,6 @@
     <h1>Welcome</h1>
     <p>Scroll down or open the navigation to get started.</p>
     <p class="small">
-        <a href="https://derpibooru.org/images/2486218" target="_blank" rel="noopener nofollow">BG by Pridark</a>
+        <a href="https://derpibooru.org/images/2793453" target="_blank" rel="noopener nofollow">BG by Pridark</a>
     </p>
 </section>
